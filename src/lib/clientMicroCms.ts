d242db6cf445560daf711f3c1fe3a663ff22b9ad
takasaki376 <takasaki.a201070@outlook.jp>
2022-08-28 17:26:53 +0900

    keyの変数名変更

--- src/lib/clientMicroCms.ts
+++ src/lib/clientMicroCms.ts
@@ -3,5 +3,5 @@ import { createClient } from "microcms-js-sdk"; //ES6
 // Initialize Client SDK.
 export const client = createClient({
   serviceDomain: "cc5wlk04tm",
-  apiKey: process.env.NEXT_PUBLIC_API_KEY || "",
+  apiKey: process.env.API_KEY || "",
 });
